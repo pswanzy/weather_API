6a41cbc727e5f6b34428d73d01ee23c504c46c94
Peter Swanzy <120614923+pswanzy@users.noreply.github.com>
2023-02-22 00:48:06 +0000

    removed other unneeded elements

--- index.js
+++ index.js
@@ -20,16 +20,16 @@ app.get("/", function(req, res) {
         const humidity = weatherData.main.humidity; // add humidity and wind speed data
         const description = weatherData.weather[0].description;
         const windSpeed = weatherData.wind.speed;
-        const icon = weatherData.weather[0].icon;
-        const imgURL = "http://openweathermap.org/img/wn/" + icon + "@2x.png";
+        //const icon = weatherData.weather[0].icon;
+        //const imgURL = "http://openweathermap.org/img/wn/" + icon + "@2x.png";
 
         //res.write("<h3>The temperature in " + query + " is " + temp + " degrees Farenheit</h3>");
         res.write("<h3>The weather in " + query + " is currently " + description + "</h3>");
         //add humidity and windspeed headings
         res.write("<h3>The humidity is " + humidity + "%</h3>");
         res.write("<h3>The current wind speed is " + windSpeed + " mph</h3>");
-        res.write("<img src=" + imgURL + ">"); //<img> html info, using imgURL
-        res.write("<br><br><h1>Now, GO OUTSIDE!</h1>");
+        //res.write("<img src=" + imgURL + ">"); //<img> html info, using imgURL
+        //res.write("<br><br><h1>Now, GO OUTSIDE!</h1>");
         res.send();
       })
     });
