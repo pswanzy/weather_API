0495ab3b3468e0e2f999b098c4d753640a7362d4
Peter Swanzy <120614923+pswanzy@users.noreply.github.com>
2023-02-22 00:35:00 +0000

    Initial commit

---
+++ index.js
@@ -0,0 +1,45 @@
+const express = require("express");
+const app = express();
+const https = require("https");
+const bodyParser = require("body-parser");
+
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.get("/", function(req, res) {
+  res.sendFile(__dirname + "/index.html");
+  app.post("/", function(req, res) {
+    const query = req.body.cityName;
+    const apiKey = "f418a32419cf73a83192c280a7b273bf";
+    const units = "imperial";
+    const url = "https://api.openweathermap.org/data/2.5/weather?q=" + query + "&appid=" + apiKey + "&units=" + units;
+    https.get(url, function(response) {
+      console.log(response.statusCode);
+      response.on("data", function(data) {
+        const weatherData = JSON.parse(data); // takes JSON string and makes object {name:"value", } pairs
+        const temp = weatherData.main.temp;
+        const humidity = weatherData.main.humidity; // add humidity and wind speed data
+        const description = weatherData.weather[0].description;
+        const windSpeed = weatherData.wind.speed;
+        const icon = weatherData.weather[0].icon;
+        const imgURL = "http://openweathermap.org/img/wn/" + icon + "@2x.png";
+
+        res.write("<h3>The temperature in " + query + " is " + temp + " degrees Farenheit</h3>");
+        res.write("<h3>The weather is currently " + description + "</h3>");
+        //add humidity and windspeed headings
+        res.write("<h3>The humidity is " + humidity + "%</h3>");
+        res.write("<h3>The current wind speed is " + windSpeed + " mph</h3>");
+        res.write("<img src=" + imgURL + ">"); //<img> html info, using imgURL
+        res.write("<br><br><h1>Now, GO OUTSIDE!</h1>");
+        res.send();
+      })
+    });
+  });
+})
+
+
+app.listen(3000, function() {
+  console.log("Server is moving fast!");
+})
+
+// API Key weather app
+// f418a32419cf73a83192c280a7b273bf
---
+++ week7_readme.txt
@@ -0,0 +1,10 @@
+Homework for Week 7 - Update weather repl to do the following steps - 
+
+x    Update the API Key as noted above, so that you are using your own key and not mine!
+x    Change index.html and index.js so that you query weather by city and not by zip code.
+x    Use units=Imperial to display in Fahrenheit.
+    Check out - https://openweathermap.org/current for ideas on querying by city!
+    Display the humidity and wind speed along with the other weather results. Keep existing code as is.
+    Review section on parameters like humidity and wind speed - https://openweathermap.org/current
+    Save the Repl and update your Github Repo for Weather with City.
+    In your Dropbox Week 7 Folder, post the URLs from ReplIt and the GitHub Repo 
